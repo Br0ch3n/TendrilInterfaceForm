5a2957855471083bdfde82a413b9afd84e344887
Br0ch3n <mbwoote@g.clemson.edu>
2020-05-20 15:33:48 -0400

    Bagan filling out new singleton class

--- TendrilInterfaceForm/TendrilStateSingleton.cs
+++ TendrilInterfaceForm/TendrilStateSingleton.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TendrilInterfaceForm
 {
-    class TendrilStateSingleton
+    public class TendrilStateSingleton
     {
+        /* ============= Members ===============*/
+        // Architecture members
+        private static TendrilStateSingleton instance = null;
 
+        // Tendril parameters
+        
 
+        /* ============= Methods ===============*/
 
+        private TendrilStateSingleton()
+        {
+            //setup and initialization
+        }
+
+        public static TendrilStateSingleton getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new TendrilStateSingleton();
+            }
+
+            return instance;
+        }
+             
     }
 }
